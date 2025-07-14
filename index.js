782fbbf0a7abfa2e6c6fb1cefc784106287ad6de
metgag <moreehigh1@gmail.com>
2025-07-14 10:19:12 +0700

    feat: added index.js to handle ./src files

---
+++ index.js
@@ -0,0 +1,24 @@
+// no. 1
+import { handlingThenCtc, handlingTryCtc } from "./src/fetch-data.js";
+// handlingThenCtc(true);
+// handlingThenCtc(false);
+
+// handlingTryCtc(true);
+// handlingTryCtc(false);
+
+// no. 2
+import { testData } from "./src/get-data.js";
+// testData(true);
+// testData(false);
+
+// no. 3
+import { getUserNameDomisili } from "./src/fetch-rest-api.js";
+// getUserNameDomisili();
+
+// no. 4
+import { divideAndSort } from "./src/divide-sort.js";
+// divideAndSort(5956560159466056);
+
+// no. 5
+import { cliNote } from "./src/cli-note.js";
+cliNote();
